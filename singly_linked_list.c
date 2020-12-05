33f03aaf1650ac23a2ae02a60fa1d0ce59a86191
kingchocomilk <gamereg911@gmail.com>
2020-12-04 19:32:26 -0700

    More changes.

--- singly_linked_list.c
+++ singly_linked_list.c
@@ -10,6 +10,9 @@ Log:
     the value to be whatever the user needs it to be.
 '''
 
+#include <stdlib.h>
+#include <assert.h>
+
 struct SinglyNode {
   void *value;
   SinglyNode *next;
@@ -17,15 +20,22 @@ struct SinglyNode {
 
 struct SinglyLinkedList {
   SinglyNode *root;
+  SinglyNode *last;
   int length;
 };
 
-void initLinkedList(struct SinglyLinkedList *newList) {
-  newList = (SinglyLinkedList *) malloc(sizeof(struct SinglyLinkedList));
-  newList->length = 0;
+void resetList(struct SinglyLinkedList *list) {
+  list->length = 0;
+  list->root = NULL;
+  list->last = root;
 }
 
-void clearLinkedList(struct SinglyLinkedList *list) {
+struct SinglyLinkedList *initList() {
+  newList = (SinglyLinkedList *) malloc(sizeof(struct SinglyLinkedList));
+  resetList(newList);
+}
+
+void clearList(struct SinglyLinkedList *list) {
   struct SinglyNode *node = list->root;
   while (node != 0) {
     struct SinglyNode *oldNode = node;
@@ -33,30 +43,63 @@ void clearLinkedList(struct SinglyLinkedList *list) {
     free(oldNode->value);
     free(oldNode);
   }
+  resetLinkedList(list);
 }
 
 '''
 Frees all nodes and its root node safely.
 '''
-void freeLinkedList(struct SinglyLinkedList *list) {
+void freeList(struct SinglyLinkedList *list) {
   clearLinkedList(list); //First clear all the nodes in the list.
   free(list); // free the whole list struct.
 }
 
-int linkedListLength(struct SinglyLinkedList *list) {
+int length(struct SinglyLinkedList *list) {
   return list->length;
 }
 
 int isEmpty(struct SinglyLinkedList *list) {
-  struct SinglyNode rootNode = list->root;
-  return rootNode === NULL;
+  assert(list->length == 0);
+  assert(list->last == list->root);
+  return list->root == NULL;
 }
 
-void add(struct SinglyLinkedList *list, void *element) {
-  if (isEmpty(list)) {
-    initSinglyNode(list->root);
+void addTo(struct SinglyLinkedList *list, void *element) {
+  if (list != 0) { // Make sure the list isn't a null pointer.
+    struct SinglyNode *newNode = (struct SinglyNode *) malloc(sizeof(struct SinglyNode));
+    if (isEmpty(list)) {
+      list->root = newNode;
+      list->root->value = element;
+    }
+    else {
+      // Go to the last of the list and add it there.
+      list->last->next = newNode;
+      list->last->next->value = element;
+      list->length++;
+      list->last = list->last->next;
+    }
   }
-  else {
-    
+}
+
+'''
+I can use a contains if the user gives a pointer to a function that
+has the semantics to see if two types are similiar.
+'''
+
+int contains(struct SinglyLinkedList *list, void *element,
+		       int (*equals_algo)(void*, void*)) {
+  struct SinglyNode currentNode = list->root;
+  while (currentNode != 0) {
+    if (currentNode->value // how do I use the function pointer?
   }
+}
+
+void remove(struct SinglyLinkedList *list, void *element,
+	    int (*equals_algo)(void*, void*)) {
+  
+}
+
+void *pop(struct SinglyLinkedList *list, void *element,
+	  int (*equals_algo)(void*, void*)) {
+  
 }
